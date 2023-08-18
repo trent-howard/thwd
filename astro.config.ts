fa32eedc3447d9cbdf5ea826bfb3d027b3dd2374
Trent Howard <trent.howard.42@gmail.com>
2023-08-18 17:11:18 +1000

    add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://thwd.dev",
-  integrations: [tailwind()],
-  compressHTML: true,
+  integrations: [tailwind(), sitemap()],
+  compressHTML: true
 });
