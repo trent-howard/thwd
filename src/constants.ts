18b08590bb51864a8959e07baa5c1507e51c2305
Trent Howard <trent.howard.42@gmail.com>
2023-08-21 16:29:55 +1000

    consolidate some constants and types

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,28 @@
-export const MENU_ITEMS = [
+export const menuItems: Array<{ title: string; path: string }> = [
   {
     title: "Home",
     path: "/",
   },
 ];
+
+export type Config = {
+  author: string;
+  title: string;
+  description: string;
+  ogLocale: string;
+};
+
+export const config: Config = {
+  author: "Trent",
+  title: "thwd.dev",
+  description:
+    "Ramblings about tech, music, and anything else that takes my fancy",
+  ogLocale: "en_AU",
+};
+
+export type Meta = {
+  title: string;
+  description?: string;
+  ogImage?: string;
+  publishedAt?: string;
+};
