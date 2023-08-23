f670fb5f76c073909c3ed3aeb3d9d7f860e16854
Trent Howard <trent.howard.42@gmail.com>
2023-08-23 10:43:16 +1000

    add aspect ratio to post hero

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,12 @@ import plugin from "tailwindcss/plugin";
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
   plugins: [
+    require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
     plugin(function ({ addComponents }) {
       addComponents({
