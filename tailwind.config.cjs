6d70dde49910cfd6774e8066a291b5ca2854193c
Trent Howard <trent.howard.42@gmail.com>
2023-08-23 15:55:25 +1000

    add css tailwind theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,33 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textColor: "rgb(var(--theme-text) / <alpha-value>)",
+        bgColor: "rgb(var(--theme-background) / <alpha-value>)",
+        primary: "rgb(var(--theme-primary) / <alpha-value>)",
+        secondary: "rgb(var(--theme-secondary) / <alpha-value>)",
+        accent: "rgb(var(--theme-accent) / <alpha-value>)",
+        accent2: "rgb(var(--theme-accent-2) / <alpha-value>)",
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "--tw-prose-body": theme("colors.textColor / 1"),
+            "--tw-prose-headings": theme("colors.textColor / 1"),
+            "--tw-prose-links": theme("colors.textColor / 1"),
+            "--tw-prose-bold": theme("colors.textColor / 1"),
+            "--tw-prose-bullets": theme("colors.accent2 / 1"),
+            "--tw-prose-counters": theme("colors.textColor / 1"),
+            "--tw-prose-quotes": theme("colors.accent2 / 1"),
+            "--tw-prose-quote-borders": theme("colors.accent2 / 1"),
+            "--tw-prose-code": theme("colors.textColor / 1"),
+            "--tw-prose-hr": `0.5px dashed ${theme("colors.textColor / 1")}`,
+            "--tw-prose-th-borders": theme("colors.textColor / 1"),
+          },
+        },
+      }),
+    },
   },
   plugins: [
     require("@tailwindcss/aspect-ratio"),
