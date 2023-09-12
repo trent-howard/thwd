98acc0b407c065481c4d0abd6d72eb02eb1df42b
Trent Howard <trent.howard.42@gmail.com>
2023-09-12 10:23:10 +1000

    remove unneded null check

--- src/utils.ts
+++ src/utils.ts
@@ -2,5 +2,5 @@ import { getCollection } from "astro:content";
 const isDev = import.meta.env.DEV;
 
 export async function getPosts() {
-  return getCollection("posts", ({ data }) => isDev || !data?.draft);
+  return getCollection("posts", ({ data }) => isDev || !data.draft);
 }
