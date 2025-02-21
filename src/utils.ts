33021a31a3acb6349c4420c8aa50a1d1d26a31cf
Trent Howard <trent.howard.42@gmail.com>
2025-02-21 22:45:27 +1100

    refactor(Posts): from collection to content layer API

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,9 @@
 import { getCollection } from "astro:content";
 const isDev = import.meta.env.DEV;
 
+/**
+ * Fetches all posts filtering drafts if the app is PROD
+ */
 export async function getPosts() {
   return getCollection("posts", ({ data }) => isDev || !data.draft);
 }
